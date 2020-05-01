a5bed2f4bae6c5a1e92b41fd831c2ae8ab947728
estanislau <estanislau.sena@gmail.com>
2020-05-01 12:06:13 -0300

    Implementado matriz de perfis

--- Modulo_Obstaculos/conversao_codigo.py
+++ Modulo_Obstaculos/conversao_codigo.py
@@ -14,23 +14,46 @@ def gerar_matriz (n_linhas, n_colunas):
     return [[" "]*n_colunas for _ in range(n_linhas)]
 
 def DetectaObstaculos(imgBin, vet):
-
-  # Variáveis e limiares
-  minimo_livre_inf = 200 # na parte de baixo da imagem
-  minimo_livre_sup = 110 # na parte de cima da imagem
-  num_perfis = 15 # quantidade de perfis a serem analisados na imagem
-
-  largura, altura, _ = imgBin.shape
-
-  print("Largura = {0} Altura = {1}".format(largura, altura))
-
-  passo_vertical = altura/num_perfis
-
-  linhas, colunas = 6, 50
-  matriz_contagem = gerar_matriz(linhas, 50)
+    
+    # Variáveis e limiares
+    minimo_livre_inf = 200 # na parte de baixo da imagem
+    minimo_livre_sup = 110 # na parte de cima da imagem
+    num_perfis = 15 # quantidade de perfis a serem analisados na imagem
+    
+    largura, altura, _ = imgBin.shape
+    
+    print("Largura = {0} Altura = {1}".format(largura, altura))
+    
+    passo_vertical = altura/num_perfis
+    
+    linhas, colunas = 6, 50
+    matriz_contagem = gerar_matriz(linhas, 50)
   
+    '''
+     matriz_contagem: primeira linha (0) contem codigo da regiao
+     1 - Black (desconhecido)
+     2 - Red (região livre, na verdade cinza)
+     3 - White (faixa)
+    
+     Segunda linha (1) contem o numero de pixels seguidos da regiao em determinado perfil da imagem.
+     Terceira linha (2) contem o ponto medio da regiao (coordenada x)
+     Quarta linha (3) contem 1 se regiao for candidata a pertencer a faixa de pedestre BR
+     Quinta linha (4) contem 1 se regiao for candidata a pertencer a faixa de pedestre BW
+     Sexta linha (5) contem 1 se regiao for candidata a pertencer a faixa de pedestre WR
+    '''  
   
-  
+    matriz_perfis = gerar_matriz(num_perfis, 6)
+    '''
+    matriz_perfis: resultados da analise de cada perfil
+    (uma linha por perfil)
+    Colunas: y, classificacao, numero de regioes R validas, ponto medio R1, ponto medio R2
+    
+    Classificacao:
+    1- livre
+    2- obstaculo
+    3- faixa
+    '''
+ 
   
   
   
