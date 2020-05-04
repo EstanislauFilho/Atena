973e7a7c3d53285da5ddc063fdc1157b43357a40
estanislau <estanislau.sena@gmail.com>
2020-05-04 08:44:36 -0300

    Implementado script para avaliação dos classificadores.

--- Videos/Placas/avaliacao_frames_video_teste_1.py
+++ Videos/Placas/avaliacao_frames_video_teste_1.py
@@ -14,13 +14,24 @@ n_placa_2, c_placa_2 = "Pedestre", cv2.CascadeClassifier('/home/estanislau/Proje
 
 classificadores = [(n_placa_1, c_placa_1), (n_placa_2, c_placa_2)]
 
-imagem = cv2.imread("/home/estanislau/Projetos/Atena/Videos/Placas/frames_video_teste_1/1000.jpg")
+numero_imagem = 1200
 
+imagem = cv2.imread("/home/estanislau/Projetos/Atena/Videos/Placas/frames_video_teste_1/"+str(numero_imagem)+".jpg")
 
+def detecta_Placas(img, nome, classificador):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_placa = classificador.detectMultiScale(img_gray, scaleFactor = 1.1, minNeighbors = 15)
+    
+    for (x,y,w,h) in img_placa:       
+        cv2.rectangle(img, (x, y), (x + w, y + h), ((0, 220, 220)), 2)
+        cv2.putText(img, nome, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 220, 220), 2)
+        
+    return img
 
+for n, c in classificadores: 
+    imagem_placas = detecta_Placas(imagem, n, c)
 
-
-cv2.imshow("Apresenta Imagens", imagem)
+cv2.imshow("Apresenta Imagens", imagem_placas)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()	
