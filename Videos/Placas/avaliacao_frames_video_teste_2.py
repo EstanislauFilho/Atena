044c8c48a956b51b27d264350d06832b7bb82cd7
estanislau <estanislau.sena@gmail.com>
2020-05-04 08:45:39 -0300

    Implementado script para avaliação dos classificadores.

--- Videos/Placas/avaliacao_frames_video_teste_2.py
+++ Videos/Placas/avaliacao_frames_video_teste_2.py
@@ -8,14 +8,30 @@ Created on Mon Apr  6 21:40:46 2020
 
 
 import cv2
-import glob
 
+n_placa_1, c_placa_1 = "Pare", cv2.CascadeClassifier('/home/estanislau/Projetos/Atena/Modulo_Placas/Classificadores/cascade_pare.xml')
+n_placa_2, c_placa_2 = "Pedestre", cv2.CascadeClassifier('/home/estanislau/Projetos/Atena/Modulo_Placas/Classificadores/cascade_pedestre.xml')
 
-for i in sorted(glob.glob('frames_video_teste_1/*.jpg')):  
-    #print(i)
-    imagem = cv2.imread(i)
+classificadores = [(n_placa_1, c_placa_1), (n_placa_2, c_placa_2)]
+
+numero_imagem = 1200
+
+imagem = cv2.imread("/home/estanislau/Projetos/Atena/Videos/Placas/frames_video_teste_2/"+str(numero_imagem)+".jpg")
+
+def detecta_Placas(img, nome, classificador):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_placa = classificador.detectMultiScale(img_gray, scaleFactor = 1.1, minNeighbors = 15)
     
-    cv2.imshow("Apresenta Imagens", imagem)
-    cv2.waitKey(1000)
+    for (x,y,w,h) in img_placa:       
+        cv2.rectangle(img, (x, y), (x + w, y + h), ((0, 220, 220)), 2)
+        cv2.putText(img, nome, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 220, 220), 2)
+        
+    return img
+
+for n, c in classificadores: 
+    imagem_placas = detecta_Placas(imagem, n, c)
+
+cv2.imshow("Apresenta Imagens", imagem_placas)
+cv2.waitKey(0)
 
 cv2.destroyAllWindows()	
