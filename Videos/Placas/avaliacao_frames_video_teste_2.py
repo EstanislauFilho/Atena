5c3ebcf52496d2cf8cc04bdb0ba4ab34841b440b
estanislau <estanislau.sena@gmail.com>
2020-05-04 22:45:35 -0300

    Implementado leitura sequencial dos frames da pasta de teste 2.

--- Videos/Placas/avaliacao_frames_video_teste_2.py
+++ Videos/Placas/avaliacao_frames_video_teste_2.py
@@ -8,21 +8,22 @@ Created on Mon Apr  6 21:40:46 2020
 
 
 import cv2
+import glob
 
 n_placa_1, c_placa_1 = "Pare", cv2.CascadeClassifier('/home/estanislau/Projetos/Atena/Modulo_Placas/Classificadores/cascade_pare.xml')
 n_placa_2, c_placa_2 = "Pedestre", cv2.CascadeClassifier('/home/estanislau/Projetos/Atena/Modulo_Placas/Classificadores/cascade_pedestre.xml')
 
 classificadores = [(n_placa_1, c_placa_1), (n_placa_2, c_placa_2)]
 
-numero_imagem = 1200
+#numero_imagem = 1009
 
-imagem = cv2.imread("/home/estanislau/Projetos/Atena/Videos/Placas/frames_video_teste_2/"+str(numero_imagem)+".jpg")
+#imagem = cv2.imread("/home/estanislau/Projetos/Atena/Videos/Placas/frames_video_teste_1/"+str(numero_imagem)+".jpg")
 
-imagem = cv2.resize(imagem, (680, 420))
+#imagem = cv2.resize(imagem, (680, 420))
 
 def detecta_Placas(img, nome, classificador):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img_placa = classificador.detectMultiScale(img_gray, scaleFactor = 1.1, minNeighbors = 15)
+    img_placa = classificador.detectMultiScale(img_gray, scaleFactor = 1.1, minNeighbors = 10, minSize=(10, 10), maxSize=(100, 100))
     
     for (x,y,w,h) in img_placa:       
         cv2.rectangle(img, (x, y), (x + w, y + h), ((0, 220, 220)), 2)
@@ -30,10 +31,31 @@ def detecta_Placas(img, nome, classificador):
         
     return img
 
-for n, c in classificadores: 
-    imagem_placas = detecta_Placas(imagem, n, c)
+cont_imagem = 1000
+try:
+    for i in sorted(glob.glob('/home/estanislau/Projetos/Atena/Videos/Placas/frames_video_teste_2/*.jpg')):  
+        imagem = cv2.imread(i)
+        
+        imagem = cv2.resize(imagem, (680, 420))
+        
+        for n, c in classificadores: 
+            imagem_placas = detecta_Placas(imagem, n, c)
+    
+    
+        print("Frame: {0}".format(cont_imagem))
+        cont_imagem += 1
+        
+        cv2.imshow("Apresenta Imagens", imagem_placas)
+        cv2.waitKey(0)
+        
+        
+        
+        if cv2.waitKey(1) & 0xFF == 27:
+            cv2.destroyAllWindows()	
+    
+except:
+    cv2.destroyAllWindows()
+    print('exitting program') 
 
-cv2.imshow("Apresenta Imagens", imagem_placas)
-cv2.waitKey(0)
-
-cv2.destroyAllWindows()	
+finally:
+    cv2.destroyAllWindows()
