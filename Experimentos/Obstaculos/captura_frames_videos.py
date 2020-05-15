ab1cfa7609f6031757e262176c602b978cac3545
estanislau <estanislau.sena@gmail.com>
2020-05-14 22:37:34 -0300

    Script para capturar frames dos videos de obstáculos.

---
+++ Experimentos/Obstaculos/captura_frames_videos.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 21:22:38 2020
+
+@author: estanislau
+"""
+
+import cv2
+
+video_numero = 10
+
+video = cv2.VideoCapture("/home/estanislau/Projetos/Atena/Videos/video_obs_"+str(video_numero)+".mp4")
+
+caminho_pasta = "frames_video_obs_"+str(video_numero)+"/"
+
+cont_imagem = 1000
+cont_execucao = 1
+
+while(True):
+    
+    verif, frame = video.read()
+    
+    if verif is True:
+        imagem = cv2.resize(frame, (680, 420))
+    
+        if cont_execucao % 10 == 0:
+            cv2.imwrite(caminho_pasta + str(cont_imagem) + ".jpg",imagem)
+            print(str(cont_imagem)+"º imagem capturada com sucesso...")
+            cont_imagem += 1
+        cont_execucao += 1 
+    else:
+        print("Todas as imagens gravadas com sucesso!")
+        break
