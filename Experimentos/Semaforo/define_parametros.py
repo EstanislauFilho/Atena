638fe4556cb6a4ce600b7ab4f108c6a50c01c192
estanislau <estanislau.sena@gmail.com>
2020-05-20 21:13:02 -0300

    Script para definir os parâmetros do algoritmo de detecção dos estados do semáforo.

--- Experimentos/Semaforo/define_parametros.py
+++ Experimentos/Semaforo/define_parametros.py
@@ -8,79 +8,49 @@ Created on Mon May  4 20:52:01 2020
 
 
 import cv2
-import glob
 import numpy as np
 
+quadros = cv2.imread("1195.jpg")
 
-caminho_pasta = '/home/estanislau/Projetos/Atena/Experimentos/Semaforo/frames_video_avaliacao/*.jpg'
+def nada(x): pass
 
+# -------------------------- Interface HSV ------------------------------------
+cv2.namedWindow("Interface_HSV")
 
-min_H_sem_vermelho = 132
-max_H_sem_vermelho = 179
+cv2.createTrackbar("Valor Minimo - H", "Interface_HSV", 0, 179, nada) #Hue
+cv2.createTrackbar("Valor Minimo - S", "Interface_HSV", 0, 255, nada) #Saturation
+cv2.createTrackbar("Valor Minimo - V", "Interface_HSV", 0, 255, nada) #Value
 
-min_S_sem_vermelho = 173
-max_S_sem_vermelho = 255
-
-min_V_sem_vermelho = 172
-max_V_sem_vermelho = 255
-
-semaforo_vermelho = [min_H_sem_vermelho, max_H_sem_vermelho, min_S_sem_vermelho, max_S_sem_vermelho, min_V_sem_vermelho, max_V_sem_vermelho]
-
-
-def hsv_semaforo_vermelho(img, semaforo_vermelho):
-    status_vermelho = False
-	
-    min_H, max_H, min_S, max_S, min_V, max_V = semaforo_vermelho
-	
-    hsv_min = np.array([min_H, min_S, min_V])
-    hsv_max = np.array([max_H, max_S, max_V])
-  
-    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-			
-    mascara = cv2.inRange(img_hsv, hsv_min, hsv_max)
-
-    contours, _ = cv2.findContours(mascara, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    	
-    for c in contours:
-        if (cv2.contourArea(c) > 400):			
-            x,y,w,h = cv2.boundingRect(c)
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            cv2.putText(img, "Sinal Vermelho", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-            status_vermelho = True
-		
-    return  status_vermelho
+cv2.createTrackbar("Valor Maximo - H", "Interface_HSV", 179, 179, nada) #Hue
+cv2.createTrackbar("Valor Maximo - S", "Interface_HSV", 255, 255, nada) #Saturation
+cv2.createTrackbar("Valor Maximo - V", "Interface_HSV", 255, 255, nada) #Value
+# -----------------------------------------------------------------------------
 
 
-cont_imagem = 1000
-quantidade_imagens = len((glob.glob(caminho_pasta)))
 
-try:     
-    for i in sorted(glob.glob(caminho_pasta)):  
-        imagem = cv2.imread(i)
-        
-        status = hsv_semaforo_vermelho(imagem, semaforo_vermelho)
-        
-        print(status)
-        
-        cv2.imshow("Apresaenta Imagem", imagem)
-        cv2.waitKey(0)
-        
-        if quantidade_imagens == 0:
-            print("Todas as imagens analisadas com sucesso!")
-            cv2.destroyAllWindows()
-            break
-        
-        if cv2.waitKey(1) & 0xFF == 27:
-            cv2.destroyAllWindows()	
-         
-        cont_imagem += 1
-         
-        cont_imagem += 1
+while(True):
     
+    quadros_hsv = cv2.cvtColor(quadros, cv2.COLOR_BGR2HSV)
     
-except KeyboardInterrupt:
-    cv2.destroyAllWindows()
-    print('Encerrar execução...') 
+    val_minimo_H = cv2.getTrackbarPos("Valor Minimo - H", "Interface_HSV")
+    val_minimo_S = cv2.getTrackbarPos("Valor Minimo - S", "Interface_HSV")
+    val_minimo_V = cv2.getTrackbarPos("Valor Minimo - V", "Interface_HSV")
     
-finally:
-    cv2.destroyAllWindows()
+    val_maximo_H = cv2.getTrackbarPos("Valor Maximo - H", "Interface_HSV")
+    val_maximo_S = cv2.getTrackbarPos("Valor Maximo - S", "Interface_HSV")
+    val_maximo_V = cv2.getTrackbarPos("Valor Maximo - V", "Interface_HSV")
+    
+    intenso = np.array([val_maximo_H, val_maximo_S, val_maximo_V])
+    fraco = np.array([val_minimo_H, val_minimo_S, val_minimo_V])
+    mascara = cv2.inRange(quadros_hsv, fraco, intenso)
+    imagem_resultado = cv2.bitwise_and(quadros, quadros, mask=mascara)
+    
+    cv2.imshow("Stream Video", quadros)
+    
+    cv2.imshow('Interface HSV OpenCV', imagem_resultado)
+    
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+
+cv2.destroyAllWindows() 
