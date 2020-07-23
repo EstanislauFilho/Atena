2e3b1c8ef12eddd40d6969dcaaa80ddbd4f2bfe8
estanislau <estanislau.sena@gmail.com>
2020-07-23 20:16:17 -0300

    Script para fazer o recorte das imagens da placa de futebol.

---
+++ Base_Dados/recorte_plc_futebol.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  5 18:06:43 2020
+
+@author: estanislau
+"""
+
+
+import cv2
+import glob
+
+
+
+caminho_pasta_original = "/home/estanislau/Projetos/Atena/Base_Dados/futebol/i/*.jpg"
+caminho_pasta_destino = "/home/estanislau/Projetos/Atena/Base_Dados/futebol/plc_futebol_original/"
+
+cont_imagem = 11129
+
+
+min_H = 117
+min_S = 89
+min_V = 0
+
+max_H = 179
+max_S = 255
+max_V = 147
+
+
+def seleciona_regiao_HSV(imagem):
+	min_HSV = (min_H, min_S, min_V)
+	max_HSV = (max_H, max_S, max_V)
+
+	imagem_hsv = cv2.cvtColor(imagem, cv2.COLOR_BGR2HSV)
+
+	mascara = cv2.inRange(imagem_hsv, min_HSV, max_HSV)
+
+	contours, hierarchy = cv2.findContours(mascara, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	
+	if contours:
+		areaMaxima = cv2.contourArea(contours[0])
+		idContorno = 0
+		i = 0
+		for cnt in contours:
+			if areaMaxima < cv2.contourArea(cnt):
+				areaMaxima = cv2.contourArea(cnt)
+				idContorno = i
+			i += 1
+		cnt = contours[idContorno]
+		x,y,w,h = cv2.boundingRect(cnt)	
+	if(areaMaxima > 2000.0):
+		imagem = imagem[y-2:y+h+2, x-2:x+w+2]
+	return imagem
+	
+
+
+print("Iniciando seleção e recorte das imagens...")
+
+for i in sorted(glob.glob(caminho_pasta_original)):
+    imagem = cv2.imread(i)
+    
+    try:
+        imagem_hsv = seleciona_regiao_HSV(imagem)
+
+    except:
+        print("Imagem {0} com problema!".format(cont_imagem))
+    
+    try:
+        cv2.imwrite(caminho_pasta_destino+str(cont_imagem)+".jpg", imagem_hsv)
+    except ValueError:
+        print("Falha no salvamento das imagens.")
+        
+    print("Recortando imagem {0}".format(cont_imagem))    
+    cont_imagem += 1
+
+print("Fim da seleção e recorte das imagens!")
