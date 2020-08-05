c5066d7cde22f8b26172ee62cbb38909e4df5baa
estanislau <estanislau.sena@gmail.com>
2020-08-05 20:46:25 -0300

    Implementado função para calcular o centro de massa da imagem.

--- Modulo_Sinalizacao_Horizontal/sinalizacao_horizontal.py
+++ Modulo_Sinalizacao_Horizontal/sinalizacao_horizontal.py
@@ -41,12 +41,39 @@ def get_perspectiva_pista(img):
     img = cv2.warpPerspective(img, matriz, (680, 420)) 
     return img
 
-def detecta_borda_direita():
-    pass
+
+def detecta_borda_direita(img):
+    img_borda_dir = img[0:420, 300:490] 
+    return img_borda_dir
 
 
-def detecta_borda_esquerda():
-    pass
+
+def calcula_centro_de_massa_imagem(img):
+    ret,thresh = cv2.threshold(img,145,250,cv2.THRESH_BINARY_INV)
+	
+    contours, hierarchy = cv2.findContours(thresh.copy(), 1, cv2.CHAIN_APPROX_NONE)
+	
+    if len(contours) > 0:
+        c = max(contours, key=cv2.contourArea)
+		
+        M = cv2.moments(c)
+        
+        cx = int(M['m10']/M['m00'])
+        
+        cy = int(M['m01']/M['m00'])
+        
+        cv2.line(img,(cx,0),(cx,420),(255,0,0),1)
+        
+        cv2.line(img,(0,cy),(680,cy),(255,0,0),1)
+        
+        cv2.drawContours(img, contours, -1, (0,255,0), 1)
+        
+        return img, cx	
+
+
+def detecta_borda_esquerda(img):
+    img_borda_esq = img[0:420, 170:360]
+    return img_borda_esq
 
 
 def detecta_faixa_pedestre():
