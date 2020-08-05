8afb5e43c58df3219f9606717a9300bfa8b1af26
estanislau <estanislau.sena@gmail.com>
2020-08-05 19:44:30 -0300

    Script para detectar a sinalização horizontal.

---
+++ Modulo_Sinalizacao_Horizontal/sinalizacao_horizontal.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+def detecta_borda_direita():
+    pass
+
+
+def detecta_borda_direita():
+    pass
+
+
+def detecta_faixa_pedestre():
+    pass
+
