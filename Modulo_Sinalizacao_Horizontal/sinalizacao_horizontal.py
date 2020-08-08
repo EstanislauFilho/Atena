13efccd80079ef161f157b638c3c7b6b7ff62d42
estanislau <estanislau.sena@gmail.com>
2020-08-08 19:42:14 -0300

    Ajustado parâmetros para detecção das faixas de pedestres.

--- Modulo_Sinalizacao_Horizontal/sinalizacao_horizontal.py
+++ Modulo_Sinalizacao_Horizontal/sinalizacao_horizontal.py
@@ -109,8 +109,8 @@ def deteccao_bordas_pista(img_borda_esq, img_borda_dir, avg_img_filtro, avg_img_
 
 def detecta_faixa_pedestre(avg_img_fil, avg_img_borda_esq, avg_img_borda_dir):
     status =  False   
-
-    if  int(avg_img_borda_esq) > 82 and int(avg_img_fil) > 40 and int(avg_img_borda_dir) > 82:
+    print(avg_img_fil, avg_img_borda_esq, avg_img_borda_dir)
+    if  int(avg_img_borda_esq) > 62 and int(avg_img_fil) > 40 and int(avg_img_borda_dir) > 62:
         status = True
     return status
    
