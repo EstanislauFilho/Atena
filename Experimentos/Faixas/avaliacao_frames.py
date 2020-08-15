76ad6ad3adedcb2063ffbc60ff14622ab61e2466
estanislau <estanislau.sena@gmail.com>
2020-08-15 20:56:34 -0300

    Atualização do script de avaliação do algoritmo.

--- Experimentos/Faixas/avaliacao_frames.py
+++ Experimentos/Faixas/avaliacao_frames.py
@@ -24,7 +24,7 @@ pontos_destino = np.float32([[pt_destino_1], [pt_destino_2], [pt_destino_3], [pt
 
 
 def perspectiva_pista(img):
-    '''
+    
     cv2.line(img, pt_pista_1, pt_pista_2, (0,0,255), 4)
     cv2.line(img, pt_pista_1, pt_pista_3, (0,0,255), 4)
     cv2.line(img, pt_pista_2, pt_pista_4, (0,0,255), 4)
@@ -34,11 +34,11 @@ def perspectiva_pista(img):
     cv2.line(img, pt_destino_1, pt_destino_3, (0,255,0), 4)
     cv2.line(img, pt_destino_2, pt_destino_4, (0,255,0), 4)
     cv2.line(img, pt_destino_3, pt_destino_4, (0,255,0), 4)
-    '''
+    
     
     matriz = cv2.getPerspectiveTransform(pontos_pista, pontos_destino)
     img = cv2.warpPerspective(img, matriz, (680, 420)) 
-    #cv2.imshow("Imga", img)
+    cv2.imshow("Imga", img)
     return img
 
 
