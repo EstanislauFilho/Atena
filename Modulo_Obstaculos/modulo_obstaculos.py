72c1b8873a4791251cc07e8178a7e068a728df9c
EstanislauFilho <estanislau.sena@gmail.com>
2020-11-16 19:54:00 -0300

    Script para detecção de obstáculos.

---
+++ Modulo_Obstaculos/modulo_obstaculos.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 16 19:08:54 2020
+
+@author: estanislau
+"""
+
+import cv2
+import glob
+import sys
+
+numero_pasta = 0
+
+caminho_pasta = '/home/estanislau/Projetos/TCC/frames_video_plc_'+str(numero_pasta)+'/*.jpg'
+
+
+try:
+    for i in sorted(glob.glob(caminho_pasta)):  
+        imagem = cv2.imread(i)
+    
+        cv2.imshow("Imagem Pista", imagem)
+        cv2.waitKey(0)
+            
+        if cv2.waitKey(1) & 0xFF == 27:
+            cv2.destroyAllWindows()	
+
+except KeyboardInterrupt:
+    cv2.destroyAllWindows()
+    sys.exit()
+    
+finally:
+    cv2.destroyAllWindows()
