e74b0f8c83f93ccf3288e0f1955a570a28ed3fbb
EstanislauFilho <estanislau.sena@gmail.com>
2020-11-16 20:17:34 -0300

    Criação das camadas de detecção.

--- Modulo_Obstaculos/modulo_obstaculos.py
+++ Modulo_Obstaculos/modulo_obstaculos.py
@@ -21,8 +21,26 @@ imagem = cv2.imread("/home/estanislau/Projetos/TCC/frames_video_plc_0/10000.jpg"
 
 
 
+for x in range(340, 679):
+    imagem[320, x] = 255, 255, 255
+    imagem[340, x] = 255, 255, 255
+    imagem[360, x] = 255, 255, 255
+    imagem[380, x] = 255, 255, 255
+    imagem[400, x] = 255, 255, 255
 
 
+for x in range(340, 0, -1):
+    imagem[320, x] = 255, 255, 255
+    imagem[340, x] = 255, 255, 255
+    imagem[360, x] = 255, 255, 255
+    imagem[380, x] = 255, 255, 255
+    imagem[400, x] = 255, 255, 255
+
+imagem[320, 340] = 0, 0, 255
+imagem[340, 340] = 0, 0, 255
+imagem[360, 340] = 0, 0, 255
+imagem[380, 340] = 0, 0, 255
+imagem[400, 340] = 0, 0, 255
 
 
 cv2.imshow("Imagem Pista", imagem)
