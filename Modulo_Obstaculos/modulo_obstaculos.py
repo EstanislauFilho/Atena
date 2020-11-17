099aaeb3a506f5f85df4c0223c66b771fef6410a
EstanislauFilho <estanislau.sena@gmail.com>
2020-11-16 21:10:01 -0300

    Detecção dos pixels da borda esquerda.

--- Modulo_Obstaculos/modulo_obstaculos.py
+++ Modulo_Obstaculos/modulo_obstaculos.py
@@ -18,54 +18,84 @@ caminho_pasta = '/home/estanislau/Projetos/TCC/frames_video_plc_'+str(numero_pas
 imagem = cv2.imread("/home/estanislau/Projetos/TCC/frames_video_plc_0/10000.jpg")
 
 
-for x in range(340, 679):
-    B0, G0, R0 = imagem[320, x] 
-    if B0  <= 254 and G0  <= 254 and R0 <= 254:
+for x in range(341, 679):
+    B0_DIR, G0_DIR, R0_DIR = imagem[320, x] 
+    if B0_DIR  <= 254 and G0_DIR  <= 254 and R0_DIR <= 254:
         imagem[320, x] = 255, 255, 255
     else:
         break
 
 
-for x in range(340, 679):
-    B1, G1, R1 = imagem[340, x]
-    if B1  <= 254 and G1  <= 254 and R1 <= 254:
+for x in range(341, 679):
+    B1_DIR, G1_DIR, R1_DIR = imagem[340, x]
+    if B1_DIR  <= 254 and G1_DIR  <= 254 and R1_DIR <= 254:
         imagem[340, x] = 255, 255, 255
     else:
         break
   
     
-for x in range(340, 679):
-    B2, G2, R2 = imagem[360, x]   
-    if B2  <= 254 and G2  <= 254 and R2 <= 254:
+for x in range(341, 679):
+    B2_DIR, G2_DIR, R2_DIR = imagem[360, x]   
+    if B2_DIR  <= 254 and G2_DIR  <= 254 and R2_DIR <= 254:
         imagem[360, x] = 255, 255, 255
     else:
         break
 
 
-for x in range(340, 679):
-    B3, G3, R3 = imagem[380, x]
-    if B3  <= 254 and G3  <= 254 and R3 <= 254:
+for x in range(341, 679):
+    B3_DIR, G3_DIR, R3_DIR = imagem[380, x]
+    if B3_DIR  <= 254 and G3_DIR  <= 254 and R3_DIR <= 254:
         imagem[380, x] = 255, 255, 255
     else:
         break
 
 
-for x in range(340, 679):
-    B4, G4, R4 = imagem[400, x]
-    if B4  <= 254 and G4  <= 254 and R4 <= 254:
+for x in range(341, 679):
+    B4_DIR, G4_DIR, R4_DIR = imagem[400, x]
+    if B4_DIR  <= 254 and G4_DIR  <= 254 and R4_DIR <= 254:
         imagem[400, x] = 255, 255, 255
     else:
         break
 
+######################################################################
 
-for x in range(340, 0, -1):
-    B0, G0, R0 = imagem[320, x]
-    B1, G1, R1 = imagem[340, x]
-    B2, G2, R2 = imagem[360, x]
-    B3, G3, R3 = imagem[380, x]
-    B4, G4, R4 = imagem[400, x]
+for x in range(339, 0, -1):
+    B0_ESQ, G0_ESQ, R0_ESQ = imagem[320, x] 
+    if B0_ESQ  <= 254 and G0_ESQ  <= 254 and R0_ESQ <= 254:
+        imagem[320, x] = 255, 255, 255
+    else:
+        break
+
+
+for x in range(339, 0, -1):
+    B1_ESQ, G1_ESQ, R1_ESQ = imagem[340, x]
+    if B1_ESQ  <= 254 and G1_ESQ  <= 254 and R1_ESQ <= 254:
+        imagem[340, x] = 255, 255, 255
+    else:
+        break
+  
     
-    if B0  >= 254 and G0  >= 254 and R0 >= 254:
+for x in range(339, 0, -1):
+    B2_ESQ, G2_ESQ, R2_ESQ = imagem[360, x]   
+    if B2_ESQ  <= 254 and G2_ESQ  <= 254 and R2_ESQ <= 254:
+        imagem[360, x] = 255, 255, 255
+    else:
+        break
+
+
+for x in range(339, 0, -1):
+    B3_ESQ, G3_ESQ, R3_ESQ = imagem[380, x]
+    if B3_ESQ  <= 254 and G3_ESQ  <= 254 and R3_ESQ <= 254:
+        imagem[380, x] = 255, 255, 255
+    else:
+        break
+
+
+for x in range(339, 0, -1):
+    B4_ESQ, G4_ESQ, R4_ESQ = imagem[400, x]
+    if B4_ESQ  <= 254 and G4_ESQ  <= 254 and R4_ESQ <= 254:
+        imagem[400, x] = 255, 255, 255
+    else:
         break
 
 for x in range(340, 679):
@@ -77,11 +107,12 @@ for x in range(340, 679):
     pass
 
 for x in range(340, 0, -1):
-    imagem[320, x] = 255, 255, 255
-    imagem[340, x] = 255, 255, 255
-    imagem[360, x] = 255, 255, 255
-    imagem[380, x] = 255, 255, 255
-    imagem[400, x] = 255, 255, 255
+    #imagem[320, x] = 255, 255, 255
+    #imagem[340, x] = 255, 255, 255
+    #imagem[360, x] = 255, 255, 255
+    #imagem[380, x] = 255, 255, 255
+    #imagem[400, x] = 255, 255, 255
+    pass
 
 imagem[320, 340] = 0, 0, 255
 imagem[340, 340] = 0, 0, 255
