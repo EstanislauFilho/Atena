286e8af50a572d6920c139cb5c14afc8a8fa2bf6
EstanislauFilho <estanislau.sena@gmail.com>
2020-11-18 21:47:21 -0300

    Implementando analisador das camadas nas imagens em escala de cinza.

--- Modulo_Obstaculos/modulo_obstaculos.py
+++ Modulo_Obstaculos/modulo_obstaculos.py
@@ -186,10 +186,15 @@ def encontra_borda_esq(img):
             x_esq_cmd_4 = x
             break 
         
-
+    print(cont_esq_cmd_0, cont_esq_cmd_1, cont_esq_cmd_2, cont_esq_cmd_3, cont_esq_cmd_4)
     return x_esq_cmd_0, y_esq_cmd_0, x_esq_cmd_1, y_esq_cmd_1, x_esq_cmd_2, y_esq_cmd_2, x_esq_cmd_3, y_esq_cmd_3, x_esq_cmd_4, y_esq_cmd_4, cont_esq_cmd_0, cont_esq_cmd_1, cont_esq_cmd_2, cont_esq_cmd_3, cont_esq_cmd_4 
 
 
+
+def detecta_obstaculos(img, x_esq_cmd_0, y_esq_cmd_0, x_esq_cmd_1, y_esq_cmd_1, x_esq_cmd_2, y_esq_cmd_2, x_esq_cmd_3, y_esq_cmd_3, x_esq_cmd_4, y_esq_cmd_4, x_dir_cmd_0, y_dir_cmd_0, x_dir_cmd_1, y_dir_cmd_1, x_dir_cmd_2, y_dir_cmd_2, x_dir_cmd_3, y_dir_cmd_3, x_dir_cmd_4, y_dir_cmd_4, cont_esq_cmd_0, cont_esq_cmd_1, cont_esq_cmd_2, cont_esq_cmd_3, cont_esq_cmd_4, cont_dir_cmd_0, cont_dir_cmd_1, cont_dir_cmd_2, cont_dir_cmd_3, cont_dir_cmd_4):
+    for x in range(x_dir_cmd_0, (x_dir_cmd_0-cont_dir_cmd_0), -1):
+        img[Y_CMD_0, x] = 255
+    
     
 
 
@@ -198,8 +203,20 @@ try:
         imagem = cv2.imread(i)
         imagem_cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
         imagem_blur = cv2.GaussianBlur(imagem_cinza, (5,5), 0)
-        imagem_tresh = cv2.inRange(imagem_blur,  220, 255) 
+        imagem_tresh = cv2.inRange(imagem_blur,  200, 255) 
     
+    
+        st_esq_cmd_0 = False
+        st_esq_cmd_1 = False
+        st_esq_cmd_2 = False
+        st_esq_cmd_3 = False
+        st_esq_cmd_4 = False
+        
+        st_dir_cmd_0 = False
+        st_dir_cmd_1 = False
+        st_dir_cmd_2 = False
+        st_dir_cmd_3 = False
+        st_dir_cmd_4 = False
 
         [
         x_esq_cmd_0, y_esq_cmd_0,
@@ -227,32 +244,113 @@ try:
         cont_dir_cmd_4 
         ] = encontra_borda_dir(imagem_tresh)
         
-        '''
-        cv2.line(imagem, (x_esq_cmd_0, y_esq_cmd_0), (340,419), (255,0,0), 2)
-        cv2.line(imagem, (x_esq_cmd_1, y_esq_cmd_1), (340,419), (255,0,0), 2)
-        cv2.line(imagem, (x_esq_cmd_2, y_esq_cmd_2), (340,419), (255,0,0), 2)
-        cv2.line(imagem, (x_esq_cmd_3, y_esq_cmd_3), (340,419), (255,0,0), 2)
-        cv2.line(imagem, (x_esq_cmd_4, y_esq_cmd_4), (340,419), (255,0,0), 2)
-        '''
         
-        if cont_dir_cmd_0 > 30 and cont_dir_cmd_0 < 300: 
+        
+        if cont_esq_cmd_0 > 10 and cont_esq_cmd_0 < 335: 
+            cv2.line(imagem, (x_esq_cmd_0, y_esq_cmd_0), (340,419), (255,0,0), 2)
+            st_esq_cmd_0 = True
+        elif cont_esq_cmd_0 > 335:
+            cv2.line(imagem, (x_esq_cmd_0, Y_CMD_0), (340,419), (255,0,0), 2)
+            st_esq_cmd_0 = True
+            
+        if cont_esq_cmd_1 > 10 and cont_esq_cmd_1 < 335:    
+           cv2.line(imagem, (x_esq_cmd_1, y_esq_cmd_1), (340,419), (255,0,0), 2)
+           st_esq_cmd_1 = True
+        elif cont_esq_cmd_1 > 335:
+            cv2.line(imagem, (x_esq_cmd_1, Y_CMD_1), (340,419), (255,0,0), 2)
+            st_esq_cmd_1 = True
+        
+        if cont_esq_cmd_2 > 10 and cont_esq_cmd_2 < 335: 
+            cv2.line(imagem, (x_esq_cmd_2, y_esq_cmd_2), (340,419), (255,0,0), 2)
+            st_esq_cmd_2 = True
+        elif cont_esq_cmd_2 >= 335:
+            cv2.line(imagem, (x_esq_cmd_2, Y_CMD_2), (340,419), (255,0,0), 2)
+            st_esq_cmd_2 = True
+        
+        
+        if cont_esq_cmd_3 > 10 and cont_esq_cmd_3 < 335:
+            cv2.line(imagem, (x_esq_cmd_3, y_esq_cmd_3), (340,419), (255,0,0), 2)
+            st_esq_cmd_3 = True
+        elif cont_esq_cmd_3 > 335:
+            cv2.line(imagem, (x_esq_cmd_3, Y_CMD_3), (340,419), (255,0,0), 2)
+            st_esq_cmd_3 = True
+        
+        if cont_esq_cmd_4 > 10 and cont_esq_cmd_4 < 335:
+            cv2.line(imagem, (x_esq_cmd_4, y_esq_cmd_4), (340,419), (255,0,0), 2)
+            st_esq_cmd_4 = True
+        elif cont_esq_cmd_4 > 335:
+            cv2.line(imagem, (x_esq_cmd_4, Y_CMD_4), (340,419), (255,0,0), 2)
+            st_esq_cmd_4 = True
+        
+        
+        
+        
+        
+        if cont_dir_cmd_0 > 10 and cont_dir_cmd_0 < 300: 
             cv2.line(imagem, (x_dir_cmd_0, y_dir_cmd_0), (340,419), (255,0,0), 2)
+            st_dir_cmd_0 = True
+        else:
+            pass
         
-        if cont_dir_cmd_1 > 30 and cont_dir_cmd_1 < 300: 
+        if cont_dir_cmd_1 > 10 and cont_dir_cmd_1 < 300: 
             cv2.line(imagem, (x_dir_cmd_1, y_dir_cmd_1), (340,419), (255,0,0), 2)
+            st_dir_cmd_1 = True
             
-        if cont_dir_cmd_2 > 30 and cont_dir_cmd_2 < 300: 
+        if cont_dir_cmd_2 > 10 and cont_dir_cmd_2 < 300: 
             cv2.line(imagem, (x_dir_cmd_2, y_dir_cmd_2), (340,419), (255,0,0), 2)
+            st_dir_cmd_2 = True
             
-        if cont_dir_cmd_3 > 30 and cont_dir_cmd_3 < 300: 
-            cv2.line(imagem, (x_dir_cmd_3, y_dir_cmd_3), (340,419), (255,0,0), 2) 
+        if cont_dir_cmd_3 > 10 and cont_dir_cmd_3 < 300: 
+            cv2.line(imagem, (x_dir_cmd_3, y_dir_cmd_3), (340,419), (255,0,0), 2)
+            st_dir_cmd_3 = True
             
-        if cont_dir_cmd_4 > 30 and cont_dir_cmd_4 < 300: 
+        if cont_dir_cmd_4 > 10 and cont_dir_cmd_4 < 300: 
             cv2.line(imagem, (x_dir_cmd_4, y_dir_cmd_4), (340,419), (255,0,0), 2)
+            st_dir_cmd_4 = True
         
+        
+        
+        
+        print()
+        if ((st_esq_cmd_0 is True and st_esq_cmd_1 is True and st_esq_cmd_2 is True and st_esq_cmd_3 is True and st_esq_cmd_4 is True) and
+            (st_dir_cmd_0 is True and st_dir_cmd_1 is True and st_dir_cmd_2 is True and st_dir_cmd_3 is True and st_dir_cmd_4 is True)):
+            detecta_obstaculos(
+                                imagem_cinza, 
+                                x_esq_cmd_0, y_esq_cmd_0, 
+                                x_esq_cmd_1, y_esq_cmd_1, 
+                                x_esq_cmd_2, y_esq_cmd_2, 
+                                x_esq_cmd_3, y_esq_cmd_3, 
+                                x_esq_cmd_4, y_esq_cmd_4, 
+                                x_dir_cmd_0, y_dir_cmd_0, 
+                                x_dir_cmd_1, y_dir_cmd_1, 
+                                x_dir_cmd_2, y_dir_cmd_2, 
+                                x_dir_cmd_3, y_dir_cmd_3, 
+                                x_dir_cmd_4, y_dir_cmd_4,
+                                cont_esq_cmd_0, 
+                                cont_esq_cmd_1, 
+                                cont_esq_cmd_2, 
+                                cont_esq_cmd_3, 
+                                cont_esq_cmd_4, 
+                                cont_dir_cmd_0, 
+                                cont_dir_cmd_1, 
+                                cont_dir_cmd_2, 
+                                cont_dir_cmd_3, 
+                                cont_dir_cmd_4 
+                               )
+            
+   
+        
+        
+        if (st_dir_cmd_0 is False and st_dir_cmd_1 is True and st_dir_cmd_2 is True and st_dir_cmd_3 is True and st_dir_cmd_4 is True):
+           pass
+
+            
+            
+            
+       
     
         cv2.imshow("Imagem Pista", imagem)
-        #cv2.imshow("Imagem Cinza", imagem_cinza)
+        cv2.imshow("Imagem Cinza", imagem_cinza)
         #cv2.imshow("Imagem Blur", imagem_blur)
         cv2.imshow("Imagem tresh", imagem_tresh)
         cv2.waitKey(0)
