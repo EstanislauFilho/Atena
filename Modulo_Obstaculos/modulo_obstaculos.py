534116f06a4a8c17a9a7339fee4bd255f4fb7fac
EstanislauFilho <estanislau.sena@gmail.com>
2020-11-21 21:01:03 -0300

    Contrução das camadas de detecção 3 e 4.

--- Modulo_Obstaculos/modulo_obstaculos.py
+++ Modulo_Obstaculos/modulo_obstaculos.py
@@ -19,31 +19,24 @@ imagem = cv2.imread("/home/estanislau/Projetos/TCC/frames_video_plc_0/10000.jpg"
 
 
 
-Y0 = 410
-Y1 = 390
-Y2 = 370
-Y3 = 350
-Y4 = 330
-Y5 = 310
-Y6 = 290
-Y7 = 270
-Y8 = 250
-Y9 = 230
-
+Y0 = 400
+Y1 = 370
+Y2 = 340
+Y3 = 310
+Y4 = 280
 
 x_dir0, y_dir0 = 0, 0
 x_dir1, y_dir1 = 0, 0
 x_dir2, y_dir2 = 0, 0
 x_dir3, y_dir3 = 0, 0
 x_dir4, y_dir4 = 0, 0
-x_dir5, y_dir5 = 0, 0
 
 x_esq0, y_esq0 = 0, 0
 x_esq1, y_esq1 = 0, 0
 x_esq2, y_esq2 = 0, 0
 x_esq3, y_esq3 = 0, 0
 x_esq4, y_esq4 = 0, 0
-x_esq5, y_esq5 = 0, 0
+
 
 
 
@@ -439,7 +432,146 @@ def detectaBordaDirCMD0(img):
     return x_Dir, y_Dir
 
 
+def deteccaoObstaculosCamada4(img, x_esq4, y_esq4, x_dir4, y_dir4):
+    #print(x_esq0, y_esq0, x_dir0, y_dir0)
+    cont_esq = 0
+    cont_dir = 0
+    
+    auxEsq1 = 1000
+    auxEsq2 = 0
+    
+    auxDir1 = 1000
+    auxDir2 = 0
 
+    for x in range(x_esq4, 339):
+        canalCoresBordaEsq4 = img[Y4, x]   
+        
+        if canalCoresBordaEsq4 >= auxEsq2:
+            auxEsq2 = canalCoresBordaEsq4
+            
+        if canalCoresBordaEsq4 < auxEsq1:
+            auxEsq1 = canalCoresBordaEsq4
+            
+        if auxEsq2 > (auxEsq1*1.20):
+            pass
+        
+        #print(auxEsq1, auxEsq2, canalCoresBordaEsq0)
+        
+        img[Y4, x] = 255
+        cont_esq += 1
+
+    
+    if x_dir4 < 679:
+        for x in range(x_dir4, 341, -1):
+            canalCoresBordaDir4 = img[Y4, x]
+            
+            if canalCoresBordaDir4 >= auxDir2:
+                auxDir2 = canalCoresBordaDir4
+                
+            if canalCoresBordaDir4 < auxDir1:
+                auxDir1 = canalCoresBordaDir4
+                
+            if auxDir2 > (auxDir1*1.20):
+                pass
+            
+            #print(auxDir1, auxDir2, canalCoresBordaDir0)
+            
+            img[Y4, x] = 255
+            cont_dir += 1
+
+def deteccaoObstaculosCamada3(img, x_esq3, y_esq3, x_dir3, y_dir3):
+    #print(x_esq0, y_esq0, x_dir0, y_dir0)
+    cont_esq = 0
+    cont_dir = 0
+    
+    auxEsq1 = 1000
+    auxEsq2 = 0
+    
+    auxDir1 = 1000
+    auxDir2 = 0
+
+    for x in range(x_esq3, 339):
+        canalCoresBordaEsq3 = img[Y3, x]   
+        
+        if canalCoresBordaEsq3 >= auxEsq2:
+            auxEsq2 = canalCoresBordaEsq3
+            
+        if canalCoresBordaEsq3 < auxEsq1:
+            auxEsq1 = canalCoresBordaEsq3
+            
+        if auxEsq2 > (auxEsq1*1.20):
+            pass
+        
+        #print(auxEsq1, auxEsq2, canalCoresBordaEsq0)
+        
+        img[Y3, x] = 255
+        cont_esq += 1
+
+    
+    if x_dir3 < 679:
+        for x in range(x_dir3, 341, -1):
+            canalCoresBordaDir3 = img[Y3, x]
+            
+            if canalCoresBordaDir3 >= auxDir2:
+                auxDir2 = canalCoresBordaDir3
+                
+            if canalCoresBordaDir3 < auxDir1:
+                auxDir1 = canalCoresBordaDir3
+                
+            if auxDir2 > (auxDir1*1.20):
+                pass
+            
+            #print(auxDir1, auxDir2, canalCoresBordaDir0)
+            
+            img[Y3, x] = 255
+            cont_dir += 1
+
+def deteccaoObstaculosCamada2(img, x_esq2, y_esq2, x_dir2, y_dir2):
+    #print(x_esq0, y_esq0, x_dir0, y_dir0)
+    cont_esq = 0
+    cont_dir = 0
+    
+    auxEsq1 = 1000
+    auxEsq2 = 0
+    
+    auxDir1 = 1000
+    auxDir2 = 0
+
+    for x in range(x_esq2, 339):
+        canalCoresBordaEsq2 = img[Y2, x]   
+        
+        if canalCoresBordaEsq2 >= auxEsq2:
+            auxEsq2 = canalCoresBordaEsq2
+            
+        if canalCoresBordaEsq2 < auxEsq1:
+            auxEsq1 = canalCoresBordaEsq2
+            
+        if auxEsq2 > (auxEsq1*1.20):
+            pass
+        
+        #print(auxEsq1, auxEsq2, canalCoresBordaEsq0)
+        
+        img[Y2, x] = 255
+        cont_esq += 1
+
+    
+    if x_dir2 < 679:
+        for x in range(x_dir2, 341, -1):
+            canalCoresBordaDir2 = img[Y2, x]
+            
+            if canalCoresBordaDir2 >= auxDir2:
+                auxDir2 = canalCoresBordaDir2
+                
+            if canalCoresBordaDir2 < auxDir1:
+                auxDir1 = canalCoresBordaDir2
+                
+            if auxDir2 > (auxDir1*1.20):
+                pass
+            
+            #print(auxDir1, auxDir2, canalCoresBordaDir0)
+            
+            img[Y2, x] = 255
+            cont_dir += 1
 
 def deteccaoObstaculosCamada1(img, x_esq1, y_esq1, x_dir1, y_dir1):
     #print(x_esq0, y_esq0, x_dir0, y_dir0)
@@ -452,7 +584,7 @@ def deteccaoObstaculosCamada1(img, x_esq1, y_esq1, x_dir1, y_dir1):
     auxDir1 = 1000
     auxDir2 = 0
 
-    for x in range(x_esq0, 339):
+    for x in range(x_esq1, 339):
         canalCoresBordaEsq1 = img[Y1, x]   
         
         if canalCoresBordaEsq1 >= auxEsq2:
@@ -487,8 +619,7 @@ def deteccaoObstaculosCamada1(img, x_esq1, y_esq1, x_dir1, y_dir1):
             
             img[Y1, x] = 255
             cont_dir += 1
-   
-        
+     
 def deteccaoObstaculosCamada0(img, x_esq0, y_esq0, x_dir0, y_dir0):
     #print(x_esq0, y_esq0, x_dir0, y_dir0)
     cont_esq = 0
@@ -564,6 +695,10 @@ try:
         
         deteccaoObstaculosCamada0(imagem_cinza, x_esq0, y_esq0, x_dir0, y_dir0)
         deteccaoObstaculosCamada1(imagem_cinza, x_esq1, y_esq1, x_dir1, y_dir1)
+        deteccaoObstaculosCamada2(imagem_cinza, x_esq2, y_esq2, x_dir2, y_dir2)
+        deteccaoObstaculosCamada3(imagem_cinza, x_esq3, y_esq3, x_dir3, y_dir3)
+        deteccaoObstaculosCamada4(imagem_cinza, x_esq4, y_esq4, x_dir4, y_dir4)
+        
         
         #cv2.imshow("Imagem Pista", imagem)
         cv2.imshow("Imagem Cinza", imagem_cinza)
