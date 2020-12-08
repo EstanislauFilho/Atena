097fa44ab99f520efb96a521dd718c50fd12b71a
EstanislauFilho <estanislau.sena@gmail.com>
2020-12-08 15:09:56 -0300

    Aprimorado função de desenho do campo de detecção.

--- Modulo_Obstaculos/modulo_obstaculos.py
+++ Modulo_Obstaculos/modulo_obstaculos.py
@@ -40,40 +40,7 @@ x_esq2, y_esq2 = 0, 0
 x_esq3, y_esq3 = 0, 0
 x_esq4, y_esq4 = 0, 0
 
-
-
-def areaDeteccao(img, x_esq0, y_esq0, x_esq1, y_esq1, x_esq2, y_esq2, x_esq3, y_esq3, x_esq4, y_esq4, x_dir0, y_dir0, x_dir1, y_dir1, x_dir2, y_dir2, x_dir3, y_dir3, x_dir4, y_dir4):
-      
-    cv2.circle(img, (x_esq0, y_esq0), 5, (255, 0, 0), 2)
-    cv2.circle(img, (x_esq1, y_esq1), 5, (255, 0, 0), 2)
-    cv2.circle(img, (x_esq2, y_esq2), 5, (255, 0, 0), 2)
-    cv2.circle(img, (x_esq3, y_esq3), 5, (255, 0, 0), 2)
-    cv2.circle(img, (x_esq4, y_esq4), 5, (255, 0, 0), 2)
    
-    cv2.circle(img, (x_dir0, y_dir0), 5, (255, 0, 0), 2)
-    cv2.circle(img, (x_dir1, y_dir1), 5, (255, 0, 0), 2)
-    cv2.circle(img, (x_dir2, y_dir2), 5, (255, 0, 0), 2)
-    cv2.circle(img, (x_dir3, y_dir3), 5, (255, 0, 0), 2)
-    cv2.circle(img, (x_dir4, y_dir4), 5, (255, 0, 0), 2)
-     
-    
-    cv2.line(img, (x_esq4, y_esq4), (x_dir4, y_dir4), (255, 0, 0), 2)
-    cv2.line(img, (x_esq3, y_esq3), (x_dir3, y_dir3), (255, 0, 0), 2)
-    cv2.line(img, (x_esq2, y_esq2), (x_dir2, y_dir2), (255, 0, 0), 2)
-    cv2.line(img, (x_esq1, y_esq1), (x_dir1, y_dir1), (255, 0, 0), 2)
-    cv2.line(img, (x_esq0, y_esq0), (x_dir0, y_dir0), (255, 0, 0), 2)
-    
-    
-    cv2.line(img, (x_esq4, y_esq4), (x_esq3, y_esq3), (255, 0, 0), 2)
-    cv2.line(img, (x_esq3, y_esq3), (x_esq2, y_esq2), (255, 0, 0), 2)
-    cv2.line(img, (x_esq2, y_esq2), (x_esq1, y_esq1), (255, 0, 0), 2)
-    cv2.line(img, (x_esq1, y_esq1), (x_esq0, y_esq0), (255, 0, 0), 2)
-    
-    cv2.line(img, (x_dir4, y_dir4), (x_dir3, y_dir3), (255, 0, 0), 2)
-    cv2.line(img, (x_dir3, y_dir3), (x_dir2, y_dir2), (255, 0, 0), 2)
-    cv2.line(img, (x_dir2, y_dir2), (x_dir1, y_dir1), (255, 0, 0), 2)
-    cv2.line(img, (x_dir1, y_dir1), (x_dir0, y_dir0), (255, 0, 0), 2)
-    
     
 def definePontosVerticais(img):
     X_LIM_E = 300
@@ -277,7 +244,6 @@ def camadasDirMetodo1(img, x, y0, y1, y2, y3, y4):
     print(xCMD4, yCMD4)
     print()
 
-
 def camadasEsqMetodo2(img):
     xp0, yp0 = xb0, yb0 = 0, Y0 
     xp1, yp1 = xb1, yb1 = 0, Y1 
@@ -471,7 +437,7 @@ def camadasEsqMetodo2(img):
     print(xCMD4, yCMD4)
     print()
     '''
-    print(xCMD0, xCMD1, xCMD2, xCMD3, xCMD4)
+    #print(xCMD0, xCMD1, xCMD2, xCMD3, xCMD4)
     return xCMD0, yCMD0, xCMD1, yCMD1, xCMD2, yCMD2, xCMD3, yCMD3, xCMD4, yCMD4
 
 def camadasDirMetodo2(img):
@@ -681,8 +647,42 @@ def camadasDirMetodo2(img):
     print(xCMD4, yCMD4)
     print()
     '''
-    print(xCMD0, xCMD1, xCMD2, xCMD3, xCMD4)
+    #print(xCMD0, xCMD1, xCMD2, xCMD3, xCMD4)
     return xCMD0, yCMD0, xCMD1, yCMD1, xCMD2, yCMD2, xCMD3, yCMD3, xCMD4, yCMD4
+
+def desenhaAreaDeteccao(img, x_esq0, y_esq0, x_esq1, y_esq1, x_esq2, y_esq2, x_esq3, y_esq3, x_esq4, y_esq4, x_dir0, y_dir0, x_dir1, y_dir1, x_dir2, y_dir2, x_dir3, y_dir3, x_dir4, y_dir4):
+      
+    cv2.circle(img, (x_esq0, y_esq0), 5, (255, 0, 0), 2)
+    cv2.circle(img, (x_esq1, y_esq1), 5, (255, 0, 0), 2)
+    cv2.circle(img, (x_esq2, y_esq2), 5, (255, 0, 0), 2)
+    cv2.circle(img, (x_esq3, y_esq3), 5, (255, 0, 0), 2)
+    cv2.circle(img, (x_esq4, y_esq4), 5, (255, 0, 0), 2)
+   
+    cv2.circle(img, (x_dir0, y_dir0), 5, (255, 0, 0), 2)
+    cv2.circle(img, (x_dir1, y_dir1), 5, (255, 0, 0), 2)
+    cv2.circle(img, (x_dir2, y_dir2), 5, (255, 0, 0), 2)
+    cv2.circle(img, (x_dir3, y_dir3), 5, (255, 0, 0), 2)
+    cv2.circle(img, (x_dir4, y_dir4), 5, (255, 0, 0), 2)
+     
+    
+    cv2.line(img, (x_esq4, y_esq4), (x_dir4, y_dir4), (255, 0, 0), 2)
+    cv2.line(img, (x_esq3, y_esq3), (x_dir3, y_dir3), (255, 0, 0), 2)
+    cv2.line(img, (x_esq2, y_esq2), (x_dir2, y_dir2), (255, 0, 0), 2)
+    cv2.line(img, (x_esq1, y_esq1), (x_dir1, y_dir1), (255, 0, 0), 2)
+    cv2.line(img, (x_esq0, y_esq0), (x_dir0, y_dir0), (255, 0, 0), 2)
+    
+    
+    cv2.line(img, (x_esq4, y_esq4), (x_esq3, y_esq3), (255, 0, 0), 2)
+    cv2.line(img, (x_esq3, y_esq3), (x_esq2, y_esq2), (255, 0, 0), 2)
+    cv2.line(img, (x_esq2, y_esq2), (x_esq1, y_esq1), (255, 0, 0), 2)
+    cv2.line(img, (x_esq1, y_esq1), (x_esq0, y_esq0), (255, 0, 0), 2)
+    
+    cv2.line(img, (x_dir4, y_dir4), (x_dir3, y_dir3), (255, 0, 0), 2)
+    cv2.line(img, (x_dir3, y_dir3), (x_dir2, y_dir2), (255, 0, 0), 2)
+    cv2.line(img, (x_dir2, y_dir2), (x_dir1, y_dir1), (255, 0, 0), 2)
+    cv2.line(img, (x_dir1, y_dir1), (x_dir0, y_dir0), (255, 0, 0), 2)
+
+
 
 try:
     for i in sorted(glob.glob(caminho_pasta)):  
@@ -705,10 +705,10 @@ try:
         #print( x, y, tamanhoLinha)
         #print(y0, y1, y2, y3, y4)
         
-        camadasEsqMetodo2(imagem_tresh)
-        camadasDirMetodo2(imagem_tresh)
-        print()
+        xEsq0, yEsq0, xEsq1, yEsq1, xEsq2, yEsq2, xEsq3, yEsq3, xEsq4, yEsq4 = camadasEsqMetodo2(imagem_tresh)
+        xDir0, yDir0, xDir1, yDir1, xDir2, yDir2, xDir3, yDir3, xDir4, yDir4 = camadasDirMetodo2(imagem_tresh)
         
+                
         
         if tamanhoLinha > 5:
             pass
@@ -716,12 +716,15 @@ try:
             #camadasDirMetodo1(imagem_tresh, (x+1), y0, y1, y2, y3, y4)
         else:
             pass
-            
+          
+
+        desenhaAreaDeteccao(imagem, xEsq0, yEsq0, xEsq1, yEsq1, xEsq2, yEsq2, xEsq3, yEsq3, xEsq4, yEsq4, xDir0, yDir0, xDir1, yDir1, xDir2, yDir2, xDir3, yDir3, xDir4, yDir4)
+
 
             
 
         
-        #cv2.imshow("Imagem Pista", imagem)
+        cv2.imshow("Imagem Pista", imagem)
         #cv2.imshow("Imagem Cinza", imagem_cinza)
         #cv2.imshow("Imagem Blur", imagem_blur)
         cv2.imshow("Imagem tresh", imagem_tresh)
