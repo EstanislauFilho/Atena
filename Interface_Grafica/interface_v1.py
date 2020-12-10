48475ce6469d4dae6dc621e411ba0be3f14663b0
EstanislauFilho <estanislau.sena@gmail.com>
2020-12-10 20:50:29 -0300

    Criado estrutura para botões de entrar e sair.

--- Interface_Grafica/interface_v1.py
+++ Interface_Grafica/interface_v1.py
@@ -8,12 +8,15 @@ Created on Wed Dec  9 20:54:18 2020
 
 import pygame
 
-largura, altura = 499, 325
+largura, altura = 500, 325
 
 cor_vermelho = (255, 0, 0)
 cor_verde = (0, 255, 0)
 cor_azul = (0, 0, 255)
 
+
+btn_entrar = [200, 210, 100, 30]
+btn_sair = [200, 250, 100, 30]
 
 pygame.init()
 
@@ -32,7 +35,8 @@ while(executa):
             executa = False
     
     fundo.fill(cor_azul)
-    pygame.draw.rect(fundo, cor_vermelho, [50, 50, 20, 20])
+    pygame.draw.rect(fundo, cor_verde, btn_entrar)
+    pygame.draw.rect(fundo, cor_vermelho, btn_sair)
     
     pygame.display.update()
 
