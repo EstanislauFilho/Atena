e059ec2fb9053b6a693e3ad4fff936d1147fc48d
EstanislauFilho <estanislau.sena@gmail.com>
2020-12-09 21:26:09 -0300

    Adicionado laço de repetição para execução do programa.

--- Interface_Grafica/interface_v1.py
+++ Interface_Grafica/interface_v1.py
@@ -15,7 +15,18 @@ pygame.init()
 pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Atena v1")
 
-pygame.display.update()
+executa = True
+
+while(executa):
+    
+    for event in pygame.event.get():
+        
+        print(event)
+        
+        if event.type == pygame.QUIT:
+            executa = False
+    
+    pygame.display.update()
 
 
 pygame.quit()
