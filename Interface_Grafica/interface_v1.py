a077e1c8fa57276704d7beb55bbe105d988eb10b
EstanislauFilho <estanislau.sena@gmail.com>
2020-12-10 21:27:28 -0300

    Colocando imagem no fundo da tela inicial.

--- Interface_Grafica/interface_v1.py
+++ Interface_Grafica/interface_v1.py
@@ -8,20 +8,25 @@ Created on Wed Dec  9 20:54:18 2020
 
 import pygame
 
-largura, altura = 500, 325
+
+largura, altura = 499, 325
 
 cor_vermelho = (255, 0, 0)
 cor_verde = (0, 255, 0)
 cor_azul = (0, 0, 255)
 
+btn_entrar = [175, 150, 150, 35]
+btn_sair = [175, 195, 150, 35]
 
-btn_entrar = [200, 210, 100, 30]
-btn_sair = [200, 250, 100, 30]
 
 pygame.init()
 
 fundo = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption("Atena v1")
+pygame.display.set_caption("Atena 2020")
+clock = pygame.time.Clock()
+
+imgFundoOriginal = pygame.image.load("/home/estanislau/Projetos/Atena/Interface_Grafica/Imagens_v1/telaInicial.png")
+
 
 executa = True
 
@@ -35,10 +40,13 @@ while(executa):
             executa = False
     
     fundo.fill(cor_azul)
+    fundo.blit(imgFundoOriginal, (0, 0))
+    
     pygame.draw.rect(fundo, cor_verde, btn_entrar)
     pygame.draw.rect(fundo, cor_vermelho, btn_sair)
     
     pygame.display.update()
+    clock.tick(60)
 
 
 pygame.quit()
