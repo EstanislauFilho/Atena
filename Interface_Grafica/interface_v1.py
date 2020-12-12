a4738a27ad3750789f6ddf9df26debc40378aad8
EstanislauFilho <estanislau.sena@gmail.com>
2020-12-11 22:39:56 -0300

    Importação das imagens das telas,

--- Interface_Grafica/interface_v1.py
+++ Interface_Grafica/interface_v1.py
@@ -15,8 +15,8 @@ cor_vermelho = (255, 0, 0)
 cor_verde = (0, 255, 0)
 cor_azul = (0, 0, 255)
 
-btn_entrar = [175, 150, 150, 35]
-btn_sair = [175, 195, 150, 35]
+btnEntrarTelaInicial = [175, 150, 150, 35]
+btnSairTelaInicial = [175, 195, 150, 35]
 
 
 pygame.init()
@@ -25,28 +25,42 @@ fundo = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Atena 2020")
 clock = pygame.time.Clock()
 
-imgFundoOriginal = pygame.image.load("/home/estanislau/Projetos/Atena/Interface_Grafica/Imagens_v1/telaInicial.png")
+imgFundoTelaInicial = pygame.image.load("/home/estanislau/Projetos/Atena/Interface_Grafica/Imagens_v1/telaInicial.png")
+imgFundoSelecionaDestino = pygame.image.load("/home/estanislau/Projetos/Atena/Interface_Grafica/Imagens_v1/telaSelecionaDestino.png")
+imgFundoConfirmaDestino = pygame.image.load("/home/estanislau/Projetos/Atena/Interface_Grafica/Imagens_v1/telaConfirmaDestino.png")
+imgFundoCarregaConfiguracoes1 = pygame.image.load("/home/estanislau/Projetos/Atena/Interface_Grafica/Imagens_v1/telaConfig1.png")
+imgFundoCarregaConfiguracoes2 = pygame.image.load("/home/estanislau/Projetos/Atena/Interface_Grafica/Imagens_v1/telaConfig2.png")
+imgFundoCarregaConfiguracoes3 = pygame.image.load("/home/estanislau/Projetos/Atena/Interface_Grafica/Imagens_v1/telaConfig3.png")
+imgFundoCarregaConfiguracoes4 = pygame.image.load("/home/estanislau/Projetos/Atena/Interface_Grafica/Imagens_v1/telaConfig4.png")
+imgFundoCarregaConfiguracoes5 = pygame.image.load("/home/estanislau/Projetos/Atena/Interface_Grafica/Imagens_v1/telaConfig5.png")
+imgFundoCarregaConfiguracoes6 = pygame.image.load("/home/estanislau/Projetos/Atena/Interface_Grafica/Imagens_v1/telaConfig6.png")
 
-configTexto = pygame.font.Font('freesansbold.ttf', 30)
 
-executa = True
+configTexto = pygame.font.Font('freesansbold.ttf', 25)
 
-while(executa):
+executaTelaInicial = True
+executaTelaSelecionaDestino = True
+executaTelaConfirmaDestino = True
+executaTelaCarregaConfiguracoes = True
+
+destinoEstadio, destinoIgreja, destinoTeatro = False, False, False
+
+while(executaTelaInicial):
     
-    for event in pygame.event.get():
-        
+    for event in pygame.event.get():       
         print(event)
         
         if event.type == pygame.QUIT:
+            executaTelaInicial = False
             pygame.quit()
-            quit()
-            executa = False
+            
+            
     
     fundo.fill(cor_azul)
-    fundo.blit(imgFundoOriginal, (0, 0))
+    fundo.blit(imgFundoTelaInicial, (0, 0))
     
-    pygame.draw.rect(fundo, cor_verde, btn_entrar)
-    pygame.draw.rect(fundo, cor_vermelho, btn_sair)
+    pygame.draw.rect(fundo, cor_verde, btnEntrarTelaInicial)
+    pygame.draw.rect(fundo, cor_vermelho, btnSairTelaInicial)
     
     textoEntrar = configTexto.render("Entrar", True, (0, 0, 0))
     fundo.blit(textoEntrar, (206, 154))
